634a8888442775509f2eefa4ba137038f6473f10
Mansour <savadmansour@gmail.com>
2023-05-21 06:12:32 -0500

    Added files to describe Space Inventor PCDU

---
+++ src/eps.c
@@ -0,0 +1,190 @@
+#include "eps.h"
+
+initEPS(){
+	//Not sure if we need this chunk
+	struct utsname info;
+	uname(&info);
+	csp_conf.hostname = info.nodename;
+	csp_conf.model = info.version;
+	csp_conf.revision = info.release;
+	csp_conf.version = CSP_VERSION;
+	csp_conf.dedup = CSP_DEDUP_OFF;
+	csp_init();
+	
+	// Initialize CAN bus
+	csp_iface_t * interface = csp_can_socketcan_init("can0", CAN_BITRATE, 0);
+	while(interface == NULL) {csp_iface_t * interface = csp_can_socketcan_init("can0", CAN_BITRATE, 0);} //This is not a good way to handle not getting a connection, implement better behavior.
+	
+	csp_rdp_set_opt(3, 10000, 5000, 1, 2000, 2); //idk if we need this line
+	
+	// I don't think this chunk is needed, but I'm keeping it here just in case
+	srand(time(NULL));
+	void serial_init(void);
+	serial_init(); //*/
+	
+	//90% sure we don't need this
+	/*
+	#if (CSP_HAVE_STDIO)
+	if (rtable && csp_rtable_check(rtable)) {
+		int error = csp_rtable_load(rtable);
+		if (error < 1) {
+			printf("csp_rtable_load(%s) failed, error: %d\n", rtable, error);
+		}
+	}
+	#endif
+	*/
+	//No idea for this one
+	//csp_bind_callback(csp_service_handler, CSP_ANY);
+	//csp_bind_callback(param_serve, PARAM_PORT_SERVER);
+	//param_command_server_init();
+	//param_schedule_server_init();
+}
+
+//Setters
+//The channel here is just a fancy way to say offset
+//channel = 0 changes the first channel, up to channel = 11
+int turn_off_delay(int time_ms, int channel){
+    if((channel > 11) | (channel < 0)){return 2;} //Check for invalid channel #
+	
+	_dfl_off_in[channel] = time_ms;
+	if(param_push_single(&dfl_off_in, channel, &_dfl_off_in[channel], VERBOSE, PCDU, TIMEOUT, CSP_VERSION))
+		{return 1;} //Connection error
+    
+	return 0;
+}
+int turn_on_delay(int time_ms, int channel){
+    if((channel > 11) | (channel < 0)){return 2;} //Check for invalid channel #
+	
+	_dfl_on_in[channel] = time_ms;
+	if(param_push_single(&dfl_on_in, channel, &_dfl_on_in[channel], VERBOSE, PCDU, TIMEOUT, CSP_VERSION))
+		{return 1;} //Connection error
+    
+	return 0;
+}
+int turn_off(int channel){
+    if((channel > 11) | (channel < 0)){return 2;} //Check for invalid channel #
+	
+	_ch_on[channel] = 0;
+	if(param_push_single(&ch_on, channel, &_ch_on[channel], VERBOSE, PCDU, TIMEOUT, CSP_VERSION))
+		{return 1;} //Connection error
+    
+	return 0;
+}
+int turn_on(int channel){
+    if((channel > 11) | (channel < 0)){return 2;} //Check for invalid channel #
+	
+	_ch_on[channel] = 0;
+	if(param_push_single(&ch_on, channel, &_ch_on[channel], VERBOSE, PCDU, TIMEOUT, CSP_VERSION) )
+		{return 1;} //Connection error
+    
+	return 0;
+}
+int set_ch_protect(int channel){
+    if((channel > 11) | (channel < 0)){return 2;} //Check for invalid channel #
+	
+	_ch_protect[channel] = 0;
+	if(param_push_single(&ch_protect, channel, &_ch_protect[channel], VERBOSE, PCDU, TIMEOUT, CSP_VERSION) )
+		{return 1;} //Connection error
+    
+	return 0;
+}
+int setWatchdog(int time_ms){
+	//Naive implementation of a watchdog
+	//The Pi should ideally use this function once every hour or so. Or every whatever unit of time.
+	//If not used after time_ms milliseconds, it will get powercycled.
+	turn_off_delay (PI_CHANNEL, time_ms);
+	turn_on_delay (PI_CHANNEL, time_ms + 1000);
+	return 0;
+}
+
+///////////////
+//Getters//////
+///////////////
+
+//I do not have the telemetry parameters defined yet so I commented out these functions. If you could check the hardware and define them, it would be epic.
+/*
+    int get_ch_ilim(int channel){
+	param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+    return _ch_ilim[channel];
+    
+	}
+    
+    int get_ch_protect(int channel){
+    param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+    return _ch_ilim[channel];
+    }
+    
+    int get_pavg(int channel){
+    param_pull_single(&pavg, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+    return _pavg[channel];
+    }
+    
+    int get_pcur(int channel){
+    param_pull_single(&pcur, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+    return _pcur[channel];
+    }
+    
+    int get_vcur(int channel){
+        int arr;
+		param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+		for(int k = 0; k < 12; k++){
+        arr.push_back(_ch_ilim[k]);
+		}
+		return arr;
+    }
+    
+    int get_icur(int channel){
+        int arr;
+		param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+		for(int k = 0; k < 12; k++){
+        arr.push_back(_ch_ilim[k]);
+		}
+		return arr;
+    }
+    
+    int get_fault(int channel){
+        int arr;
+		param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+		for(int k = 0; k < 12; k++){
+        arr.push_back(_ch_ilim[k]);
+		}
+		return arr;
+    }
+    
+    int get_energy(int channel){
+        int arr;
+		param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+		for(int k = 0; k < 12; k++){
+        arr.push_back(_ch_ilim[k]);
+		}
+		return arr;
+    }
+    
+    int get_vmin(int channel){
+        int arr;
+		param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+		for(int k = 0; k < 12; k++){
+        arr.push_back(_ch_ilim[k]);
+		}
+		return arr;
+    }
+    
+    int get_vmax(int channel){
+        int arr;
+		param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+		for(int k = 0; k < 12; k++){
+        arr.push_back(_ch_ilim[k]);
+		}
+		return arr;
+    }
+    
+    int get_imax(int channel){
+        int arr;
+		param_pull_single(&ch_ilim, 0, VERBOSE, PCDU, TIMEOUT, CSP_VERSION);
+		for(int k = 0; k < 12; k++){
+        arr.push_back(_ch_ilim[k]);
+		}
+		return arr;
+    }
+	
+//*/
---
+++ src/eps.h
@@ -0,0 +1,91 @@
+// Author: Mansour Savadogo
+
+#ifndef EPS_DRIVER_POWERBOARD_H
+#define EPS_DRIVER_POWERBOARD_H
+
+#include <math.h>
+#include <pthread.h>
+#include <time.h>
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/utsname.h>
+
+#include <param/param.h>
+#include <param/param_list.h>
+#include <param/param_client.h>
+#include <vmem/vmem.h>
+
+#include <csp/csp.h>
+#include <csp/drivers/can_socketcan.h>
+#include <csp/csp_rtable.h>
+
+
+// HARDWARE PARAMETERS SET AS DEFINE STATEMENTS
+// THOSE SHOULD NEVER NEED TO CHANGE DURING RUNTIME
+#define PCDU 3
+#define CSP_VERSION 2
+#define VERBOSE 0
+#define TIMEOUT 100
+#define ARRAY_SIZE 12
+#define ARRAY_STEP 0
+#define CAN_BITRATE  1000000
+#define TIMEOUT 100
+
+// Now defining parameters. Again, those should never need to change during runtime.
+// Just check that they were set up correctly here. CSH is your friend.
+
+// Telemetry parameters
+static uint32_t _tlm_pavg[ARRAY_SIZE]; PARAM_DEFINE_REMOTE_DYNAMIC(421, tlm_pavg, PCDU, PARAM_TYPE_UINT32, ARRAY_SIZE, ARRAY_STEP, PM_CONF, &_tlm_pavg, "");
+static uint16_t _temp0; PARAM_DEFINE_REMOTE_DYNAMIC(400, temp0, PCDU, PARAM_TYPE_UINT16, 1, ARRAY_STEP, PM_CONF, &_temp0, "");
+static uint16_t _vbatt; PARAM_DEFINE_REMOTE_DYNAMIC(402, vbatt, PCDU, PARAM_TYPE_UINT16, 1, ARRAY_STEP, PM_CONF, &_vbatt, "");
+
+//System control parameters
+static uint8_t _ch_on[ARRAY_SIZE]; PARAM_DEFINE_REMOTE_DYNAMIC(20, ch_on, PCDU, PARAM_TYPE_UINT8, ARRAY_SIZE, ARRAY_STEP, PM_CONF, &_ch_on, "");
+static uint8_t _ch_protect[ARRAY_SIZE]; PARAM_DEFINE_REMOTE_DYNAMIC(201, ch_protect, PCDU, PARAM_TYPE_UINT8, ARRAY_SIZE, ARRAY_STEP, PM_CONF, &_ch_protect, "");
+static uint16_t _dfl_on_in[ARRAY_SIZE]; PARAM_DEFINE_REMOTE_DYNAMIC(236, dfl_on_in, PCDU, PARAM_TYPE_UINT16, ARRAY_SIZE, ARRAY_STEP, PM_CONF, &_dfl_on_in, "");
+static uint16_t _dfl_off_in[ARRAY_SIZE]; PARAM_DEFINE_REMOTE_DYNAMIC(237, dfl_off_in, PCDU, PARAM_TYPE_UINT16, ARRAY_SIZE, ARRAY_STEP, PM_CONF, &_dfl_off_in, "");
+
+// TO POWERCYCLE, SET POWERCYCLE PARAMETER TO 123
+
+    initEPS();
+
+    //Getters
+	/*
+    int get_vbatt(int node);
+    int get_temp(int node);
+    //Array getters. Not implemented yet as of 08/24/202
+    int get_ch_ilim(int channel);
+    int get_ch_protect(int channel);
+    int get_pavg(int channel);
+    int get_pcur(int channel);
+    int get_vcur(int channel);
+    int get_icur(int channel);
+    int get_fault(int channel);
+    int get_energy(int channel);
+    int get_vmin(int channel);
+    int get_vmax(int channel);
+    int get_imax(int channel);
+	*/
+    
+	//Setters
+    //Setters return 0 on successful communication
+    //Return 1 if unsuccessful
+	//Return 2 if invalid input
+    int turn_off_delay(int time_ms, int channel);
+    int turn_on_delay(int time_ms, int channel);
+    int turn_off(int channel);
+    int turn_on(int channel);
+    int set_ch_protect(int channel);
+    int setWatchdog(int time_ms);
+
+//Defining hardware setup for subsystems
+//Again, those should never need to change during runtime
+#define PI_CHANNEL 0
+#define GPS_CHANNEL
+#define CUBEADCS_CHANNEL
+#define HAFX1_CHANNEL
+//define the others
+
+#endif
